ce8a01097ae0def05e4472c6ccf6a728ab707609
linh <ngl.linh@gmail.com>
2016-12-03 20:24:28 +0000

    Todo

--- Main.hs
+++ Main.hs
@@ -13,6 +13,12 @@ module Main(main)
     import Data.StarField
     import Data.Moon
 
+    --Todo
+    --Finish collision detection algorithm, clustering and individual objects
+    --Animated sprites
+    --Play animation, e.g explosion, rockets...
+    --Main menu, and scoreboard...
+
     stepGame :: WorldState -> WorldState
     stepGame ws = ws'
       where player = ws_player ws
